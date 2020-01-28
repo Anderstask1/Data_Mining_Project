e0ae3de3784f23a58568391357ca3bad0457c4f0
mariuhbr <mariuhbr@stud.ntnu.no>
2020-01-28 14:05:05 +0100

    added clustering of transactions with kMeans, 15 clusters. results saved in cluster_results.txt

--- src/main.py
+++ src/main.py
@@ -1,4 +1,9 @@
 import csv
+from sklearn.cluster import KMeans
+from sklearn import metrics	
+from time import time
+import numpy as np
+import collections
 
 def load_data(path):
 	'''
@@ -64,15 +69,74 @@ def applymap(transaction, map_):
 	'''
 	ret = []
 	for item in transaction:
+
+		if item not in map_.keys(): #assign -1 to items nonexistent in transactions
+			ret.append(-1)
+
 		ret.append(map_[item])
 	return ret
 
 
+
+
+
+
+
+clusters = 15
+
+def bench_k_means(estimator, name, data):
+	t0 = time()
+	estimator.fit(data)
+	print('%-9s\t%.2fs\t%i\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f'
+          % (name, (time() - t0), estimator.inertia_,
+             metrics.homogeneity_score(labels, estimator.labels_),
+             metrics.completeness_score(labels, estimator.labels_),
+             metrics.v_measure_score(labels, estimator.labels_),
+             metrics.adjusted_rand_score(labels, estimator.labels_),
+             metrics.adjusted_mutual_info_score(labels,  estimator.labels_),
+             metrics.silhouette_score(data, estimator.labels_,
+                                      metric='euclidean',
+                                      sample_size=sample_size)))
+
+
+
 if __name__=='__main__':
-	data_path = '/home/anderstask1/Documents/Kyb/DataMining/Data_Mining_Project/datasets/groceries.csv'
+	data_path = '../datasets/groceries.csv'
 	transactions, items = load_data(data_path)
 	map_, reverse_map = create_map(items)
 	# pickle.dump(reverse_map, open('reverse_map.pkl', 'wb+'))
 	one_itemset = [[itemset] for itemset in items]
 	items_mapped = [applymap(itemset, map_) for itemset in one_itemset]
 	transactions_mapped = [applymap(transaction, map_) for transaction in transactions]
+
+	n_samples = 9835
+	n_features = 169
+	
+
+	#create boolean matrix
+	data_matrix = np.zeros((n_samples,n_features))
+	for sample in range(len(transactions_mapped)):
+		for item in range(len(transactions_mapped[sample])):
+			data_matrix[sample][item] = 1
+
+	#bench_k_means(KMeans(init='k-means++', n_clusters=clusters, n_init=10), name="k-means++", data=data_matrix)
+	#bench_k_means(KMeans(init='random', n_clusters=clusters, n_init=10), name="random", data=data_matrix)
+
+	clusterer = KMeans(init='k-means++', n_clusters=clusters, n_init=10)
+	clusterer.fit(data_matrix)          #do clustering
+
+	results = clusterer.predict(data_matrix)
+
+
+	with open('../results/cluster_results.txt', 'w') as f:
+	    for item in results:
+	        f.write("%s\n" % item)
+
+
+	counter = collections.Counter(results)
+
+	print(counter)
+
+
+
+
